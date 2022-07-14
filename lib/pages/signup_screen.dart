79b3141cd66f61236e67d099a8505906fb26c4d9
miranas11 <anasmir24@gmail.com>
2022-07-14 11:10:47 +0530

    Small changews

--- lib/pages/signup_screen.dart
+++ lib/pages/signup_screen.dart
@@ -3,44 +3,6 @@ import 'package:dating/components/constants.dart';
 import 'package:dating/widgets/input_data.dart';
 import 'package:dating/widgets/signin_button.dart';
 import 'package:flutter_tags/flutter_tags.dart';
-// import 'package:geolocator/geolocator.dart';
-
-// Future<Position> _determinePosition() async {
-//   bool serviceEnabled;
-//   LocationPermission permission;
-
-//   // Test if location services are enabled.
-//   serviceEnabled = await Geolocator.isLocationServiceEnabled();
-//   if (!serviceEnabled) {
-//     // Location services are not enabled don't continue
-//     // accessing the position and request users of the
-//     // App to enable the location services.
-//     return Future.error('Location services are disabled.');
-//   }
-
-//   permission = await Geolocator.checkPermission();
-//   if (permission == LocationPermission.denied) {
-//     permission = await Geolocator.requestPermission();
-//     if (permission == LocationPermission.denied) {
-//       // Permissions are denied, next time you could try
-//       // requesting permissions again (this is also where
-//       // Android's shouldShowRequestPermissionRationale
-//       // returned true. According to Android guidelines
-//       // your App should show an explanatory UI now.
-//       return Future.error('Location permissions are denied');
-//     }
-//   }
-
-//   if (permission == LocationPermission.deniedForever) {
-//     // Permissions are denied forever, handle appropriately.
-//     return Future.error(
-//         'Location permissions are permanently denied, we cannot request permissions.');
-//   }
-
-//   // When we reach here, permissions are granted and we can
-//   // continue accessing the position of the device.
-//   return await Geolocator.getCurrentPosition();
-// }
 
 double height = 0;
 double width = 0;
@@ -279,6 +241,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                   text: "Location",
                   fieldText: 'Enter Location',
                   iconData: Icons.location_on_outlined,
+                  ontap: () {},
                 )
               ],
             )),
